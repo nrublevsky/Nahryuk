ac9b2bbb8d66d26a6b784b03d740a4827b71d95a
CrazyDuck8 <118855672+CrazyDuck8@users.noreply.github.com>
2023-01-07 18:59:20 +0200

    Added pickup

--- LDjam52/Assets/Scripts/PlayerControls.cs
+++ LDjam52/Assets/Scripts/PlayerControls.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -9,10 +10,11 @@ public class PlayerControls : MonoBehaviour
     public float horizontalInput;
     public float speed = 20.0f;
     public float verticalInput;
+ 
     // Start is called before the first frame update
     void Start()
     {
-
+     
     }
 
     // Update is called once per frame
@@ -22,26 +24,6 @@ public class PlayerControls : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
-        
-    }
 
-   // private void OnCollisionEnter(Collision c)
-   // {
-    //    c.transform.parent = gameObject.transform;
-   // }
-
-   // public void OnCollisionEnter(Collision col)
-   // {
-    //    if (col.transform.tag == "Player")
-    //    {
-            //transform.parent = col.transform;
-    //    }
-  //  }
-
-          private void OnTriggerEnter(Collider other)
-    {
-
-        //Destroy(other.gameObject);
-        transform.parent = other.transform; 
     }
 }
