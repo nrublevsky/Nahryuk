51f374438c58ead190f630183ab958fc109d0a55
nrublevsky <nickrublevsky@gmail.com>
2023-01-07 16:03:26 +0100

    Spawner
    doing spawner

--- LDjam52/Assets/GameManager.cs
+++ LDjam52/Assets/GameManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,10 +12,13 @@ public class GameManager : MonoBehaviour
     
     public List<Vector3> spawnPositions;
 
+    public Transform randomPosition;
+    public float spawnRate;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnVegetable());
     }
 
     // Update is called once per frame
@@ -21,4 +26,16 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public IEnumerator SpawnVegetable()
+    {
+        yield return new WaitForSeconds(spawnRate);
+        spawnRate = Random.Range(1, 2);
+
+        Instantiate(spwnblVeges[Random.Range(0, spwnblVeges.Count - 1)], spawnPositions[Random.Range(0, spawnPositions.Count-1)],this.transform.rotation);
+        
+        
+    }
+
+    
 }
