82abbf71dae5dfbd77c5d31267cdc94c82a0a6bf
CrazyDuck8 <118855672+CrazyDuck8@users.noreply.github.com>
2023-01-07 21:39:15 +0200

    Not finished Conveyor, half way there
    Not finished Conveyor, half way there

---
+++ LDjam52/Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ConveyorScript : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotspeed;
+    public float speed;
+    float WPradius = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position,transform.position) < WPradius) 
+        
+        {
+            current++;
+            if(current>=waypoints.Length) 
+            
+            {
+                current = 0; 
+            
+            }
+        
+        }
+        transform.position= Vector3.MoveTowards(transform.position, waypoints[current].transform.position,Time.deltaTime * speed);
+    }
+
+   
+
+
+}
