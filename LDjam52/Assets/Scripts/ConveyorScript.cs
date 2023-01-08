8fabb4b1a70695afb696e82a3e2505684ba25099
CrazyDuck8 <118855672+CrazyDuck8@users.noreply.github.com>
2023-01-08 21:02:28 +0200

    DO NOT USE

--- LDjam52/Assets/Scripts/ConveyorScript.cs
+++ LDjam52/Assets/Scripts/ConveyorScript.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class ConveyorScript : MonoBehaviour
 {
 
@@ -12,37 +13,85 @@ public class ConveyorScript : MonoBehaviour
     float rotspeed;
     public float speed;
     float WPradius = 1;
+    private float ConveyorInterval;
+    private float COnveyorPause = 5.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(waypoints[current].transform.position,transform.position) < WPradius) 
-        
+
+
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+
         {
             current++;
-            if(current>=waypoints.Length) 
-            
+            if (current >= waypoints.Length)
+
             {
-                current = 0; 
-            
+                current = 0;
+
             }
-        
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if (ConveyorInterval > 0)
+        //   {
+        // ConveyorInterval -= Time.deltaTime;
+        //}
+        // if (ConveyorInterval <= 0)
+        // {
+
+        //   ConveyorInterval = COnveyorPause;
+        // transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+        //Invoke("Spawndoggy", timer);
+        // }
+
+        // if (Vector3.Distance(waypoints[current].transform.position,transform.position) < WPradius) 
+
+        // {
+        //   current++;
+        //   if(current>=waypoints.Length) 
+
+        //  {
+        //    current = 0; 
+
+        //  }
+
+        //  }
+        //  transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
 
     }
 
-   
-    void MovingConveyor()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
+    void ConveyourMove() 
+        
+        {
+             transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
+
+
+        }
 
     }
 
-}
